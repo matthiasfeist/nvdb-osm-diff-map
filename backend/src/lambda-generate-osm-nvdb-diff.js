ebf4bf85d98aaae426d1d0766204938691736e64
Matthias Feist <matthiasf@spotify.com>
2021-03-21 10:19:29 +0100

    generating vector tiles with tippecanoe

--- backend/src/lambda-generate-osm-nvdb-diff.js
+++ backend/src/lambda-generate-osm-nvdb-diff.js
@@ -2,6 +2,7 @@ const trafikverketData = require('./trafikverket-data.json')
 const AWS = require('aws-sdk')
 const lambda = new AWS.Lambda()
 const s3 = new AWS.S3()
+const ec2 = new AWS.EC2()
 const { processOsm } = require('./generate-osm-nvdb-diff/process')
 
 exports.handler = async function (event, context) {
@@ -17,6 +18,7 @@ exports.handler = async function (event, context) {
     event?.['detail-type'] === 'Scheduled Event'
   ) {
     await executeLambdasToGenerateDiffs(selfFunctionName)
+    await startEc2WithTippecanoe()
   }
 
   if (event?.action === 'generateOsmNvdbDiffs') {
@@ -24,6 +26,60 @@ exports.handler = async function (event, context) {
   }
 
   return event
+}
+
+async function startEc2WithTippecanoe() {
+  const BUCKET_NAME = process.env.BUCKET_NAME
+
+  const userData = [
+    '#!/bin/bash',
+    'sleep 300', // so that the lambda functions have some time to finish
+    'shutdown -h +120', // 2 hours. just to make sure we're not running an expensive server forever in case something goes wrong
+    'yum update -y',
+    'yum install git zlib zlib-devel gcc-c++ sqlite-devel -y',
+    'git clone --depth 1 https://github.com/mapbox/tippecanoe.git',
+    'cd tippecanoe/',
+    'make install',
+    'cd ..',
+    `aws s3 sync s3://${BUCKET_NAME}/tiles/json ./json/`,
+    'mkdir pbf',
+    'tippecanoe -e pbf/ --maximum-zoom=8 --minimum-zoom=4 --read-parallel --drop-densest-as-needed --generate-ids --force json/*.json',
+    `aws s3 rm s3://${BUCKET_NAME}/tiles/pbf --recursive`,
+    `aws s3 sync ./pbf/ s3://${BUCKET_NAME}/tiles/pbf --acl public-read --content-encoding gzip`,
+    'shutdown -h',
+  ]
+
+  const params = {
+    ImageId: 'ami-02a6bfdcf8224bd77', // Amazon Linux 2 AMI
+    InstanceType: 'c5.xlarge',
+    KeyName: 'test2',
+    MaxCount: 1,
+    MinCount: 1,
+    InstanceInitiatedShutdownBehavior: 'terminate',
+    IamInstanceProfile: {
+      Arn:
+        'arn:aws:iam::071192883413:instance-profile/Ec2InstanceProfileForDataConversion',
+    },
+    Monitoring: { Enabled: true },
+    UserData: Buffer.from(userData.join('\n')).toString('base64'),
+    BlockDeviceMappings: [
+      {
+        DeviceName: '/dev/xvda',
+        Ebs: {
+          DeleteOnTermination: true,
+          VolumeSize: 8,
+          VolumeType: 'gp2',
+        },
+      },
+    ],
+    TagSpecifications: [
+      {
+        ResourceType: 'instance',
+        Tags: [{ Key: 'Name', Value: 'TileRender-Tippecanoe' }],
+      },
+    ],
+  }
+  await ec2.runInstances(params).promise()
 }
 
 async function executeLambdasToGenerateDiffs(selfFunctionName) {
@@ -41,7 +97,6 @@ async function executeLambdasToGenerateDiffs(selfFunctionName) {
         }),
       })
       .promise()
-    await sleep(300)
   }
 }
 
